73abeef2c9b434e9fa97d2cc7b557573578fd8a3
SteTessari <stephanietessare@gmail.com>
2024-07-08 11:18:28 -0300

    Refactor: Added email field to the "users" table

--- src/main/java/com/example/MoneyMind/entidades/Users.java
+++ src/main/java/com/example/MoneyMind/entidades/Users.java
@@ -2,6 +2,7 @@ package com.example.MoneyMind.entidades;
 
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -18,9 +19,18 @@ public class Users {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID", unique = true)
     private Long id;
     @NotBlank
+    @Column(name = "USERNAME", unique = true)
     private String username;
+    @Email(message = "Por favor informe uma email válido")
+    @Column(name = "EMAIL")
+    private String email;
     @NotBlank
+    @Column(name = "PASSWORD")
     private String password;
+
+
+
 }
