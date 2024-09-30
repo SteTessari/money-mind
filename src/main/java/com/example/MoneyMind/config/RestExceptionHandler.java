4cb9c4988c06f7208193fc61610688553900ddb1
SteTessari <stephanietessare@gmail.com>
2024-09-30 15:43:17 -0300

    Added methods in the RestExceptionHandler class

--- src/main/java/com/example/MoneyMind/config/RestExceptionHandler.java
+++ src/main/java/com/example/MoneyMind/config/RestExceptionHandler.java
@@ -1,7 +1,10 @@
 package com.example.MoneyMind.config;
 
+import com.example.MoneyMind.config.exception.MoneyMindException;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -15,23 +18,43 @@ import java.util.Map;
 @RestControllerAdvice
 public class RestExceptionHandler {
 
-
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public Map<String, String> handleValidationExceptions(
-            MethodArgumentNotValidException ex) {
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
         Map<String, String> errors = new HashMap<>();
+
         ex.getBindingResult().getAllErrors().forEach((error) -> {
             String fieldName = ((FieldError) error).getField();
             String errorMessage = error.getDefaultMessage();
-
-            if (errorMessage != null && errorMessage.equals("must not be null"))
-                errorMessage = "Null field not allowed";
-
             errors.put(fieldName, errorMessage);
         });
 
         return errors;
     }
 
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<?> handleResourceAccessDeniedException(AccessDeniedException e) {
+        log.error(e.getMessage());
+        return createErrorResponse(HttpStatus.FORBIDDEN, e.getMessage());
+    }
+
+    @ExceptionHandler(MoneyMindException.class)
+    public ResponseEntity<?> handleResourceMoneyMindException(MoneyMindException e) {
+        log.error(e.getMessage());
+        return createErrorResponse(HttpStatus.valueOf(e.getCode().value()), e.getMessage());
+    }
+
+    @ExceptionHandler(Throwable.class)
+    public ResponseEntity<?> handleResourceThrowableException(Throwable e) {
+        log.error(e.getMessage());
+        return createErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
+    }
+
+    private ResponseEntity<Map<String, Object>> createErrorResponse(HttpStatus status, String message) {
+        Map<String, Object> body = new HashMap<>();
+        body.put("status", status.value());
+        body.put("error", status.getReasonPhrase());
+        body.put("message", message);
+        return ResponseEntity.status(status).body(body);
+    }
 }
