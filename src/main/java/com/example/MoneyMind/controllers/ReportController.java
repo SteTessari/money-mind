9b8bf6ccb214f0e3a220ef4983ba791d1f37aeb7
SteTessari <stephanietessare@gmail.com>
2024-10-09 12:33:56 -0300

    Refactor: Adjusting endpoint for fetching total balance

--- src/main/java/com/example/MoneyMind/controllers/ReportController.java
+++ src/main/java/com/example/MoneyMind/controllers/ReportController.java
@@ -4,6 +4,7 @@ import com.example.MoneyMind.config.exception.MoneyMindException;
 import com.example.MoneyMind.config.security.TokenService;
 import com.example.MoneyMind.dtos.authentication.JwtTokenDTO;
 import com.example.MoneyMind.service.ReportService;
+import com.example.MoneyMind.service.utils.JwtTokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,31 +13,17 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import static com.example.MoneyMind.config.security.TokenService.extractUserId;
-
 @RestController
 @RequestMapping("/report")
-public class ReportController {
+public class ReportController extends JwtTokenService {
 
     @Autowired
     private ReportService reportService;
-    @Autowired
-    private TokenService tokenService;
 
     @GetMapping("/total-balance")
     public ResponseEntity<?> findTotalBalance(@RequestHeader("Authorization") String authorizationHeader) {
+        JwtTokenDTO jwtTokenDTO = getJwtTokenDTO(authorizationHeader);
+        return ResponseEntity.ok(reportService.findTotalBalance(jwtTokenDTO.getId()));
 
-        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
-            String token = authorizationHeader.substring(7);
-            try {
-                JwtTokenDTO userDetails = tokenService.validateToken(token);
-
-                return ResponseEntity.ok(reportService.findTotalBalance(userDetails.getId()));
-            } catch (MoneyMindException e) {
-                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Token inválido.");
-            }
-        } else {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Cabeçalho de autorização não encontrado.");
-        }
     }
 }
