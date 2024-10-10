ba90cf9c8a8265e8bffa50381c945cac5e78db2c
SteTessari <stephanietessare@gmail.com>
2024-10-10 18:53:51 -0300

    Chore: Removing comments from the code.

--- src/main/java/com/example/MoneyMind/config/security/TokenService.java
+++ src/main/java/com/example/MoneyMind/config/security/TokenService.java
@@ -5,11 +5,11 @@ import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.auth0.jwt.interfaces.DecodedJWT;
+import com.example.MoneyMind.config.exception.MoneyMindException;
 import com.example.MoneyMind.dtos.authentication.JwtTokenDTO;
 import com.example.MoneyMind.entidades.Users;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
@@ -24,19 +24,17 @@ public class TokenService {
     @Value("${api.security.token.secret}")
     private String secret;
 
-    private final ObjectMapper objectMapper = new ObjectMapper(); // Para conversão JSON
 
     public String generateToken(Users user) {
         try {
             Algorithm algorithm = Algorithm.HMAC256(secret);
 
-            // Define o cabeçalho personalizado
             Map<String, Object> headerClaims = new HashMap<>();
-            headerClaims.put("typ", "JWT"); // Tipo do token
-            headerClaims.put("alg", "HS256"); // Algoritmo
+            headerClaims.put("typ", "JWT");
+            headerClaims.put("alg", "HS256");
 
             return JWT.create()
-                    .withHeader(headerClaims) // Adiciona o cabeçalho personalizado
+                    .withHeader(headerClaims)
                     .withIssuer("login-auth-api")
                     .withClaim("idUser", user.getId())
                     .withClaim("email", user.getEmail())
@@ -45,7 +43,7 @@ public class TokenService {
                     .sign(algorithm);
 
         } catch (JWTCreationException e) {
-            throw new RuntimeException("Erro ao gerar o token.", e);
+            throw new MoneyMindException(HttpStatus.INTERNAL_SERVER_ERROR, "Error generating token.");
         }
     }
 
@@ -59,46 +57,16 @@ public class TokenService {
                     .build()
                     .verify(token);
 
-            // Extrai as claims personalizadas
             Long idUser = decodedJWT.getClaim("idUser").asLong();
             String email = decodedJWT.getClaim("email").asString();
             String username = decodedJWT.getClaim("username").asString();
 
-            // Cria e retorna o DTO
             return new JwtTokenDTO(idUser, username, email);
 
         } catch (JWTVerificationException e) {
-            return null; // Ou lançar uma exceção personalizada
+            return null;
         }
     }
-
-
-    public static String extractUsername(String token) {
-        try {
-            DecodedJWT decodedJWT = JWT.decode(token);
-            return decodedJWT.getClaim("username").asString();
-        } catch (Exception e) {
-            return null; // Ou lançar uma exceção personalizada
-        }
-    }
-    public static Long extractUserId(String token) {
-        try {
-            DecodedJWT decodedJWT = JWT.decode(token);
-            String idUser = decodedJWT.getClaim("idUser").asString();
-            return Long.valueOf(idUser);
-        } catch (Exception e) {
-            return null; // Ou lançar uma exceção personalizada
-        }
-    }
-    public static String extractEmail(String token) {
-        try {
-            DecodedJWT decodedJWT = JWT.decode(token);
-            return decodedJWT.getClaim("email").asString();
-        } catch (Exception e) {
-            return null; // Ou lançar uma exceção personalizada
-        }
-    }
-
 
     private Instant generateExpirationDate() {
         return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
@@ -109,7 +77,7 @@ public class TokenService {
             DecodedJWT decodedJWT = JWT.decode(token);
             return decodedJWT.getExpiresAt().before(new java.util.Date());
         } catch (Exception e) {
-            return true; // Se ocorrer um erro, considera o token expirado
+            return true;
         }
     }
 }
