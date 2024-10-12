5732f1af5f6e8f6eba7e0e113ccfd265da8120e5
SteTessari <stephanietessare@gmail.com>
2024-10-12 11:14:10 -0300

    Refactor: Added notes to validate password when creating user

--- src/main/java/com/example/MoneyMind/dtos/UserDTO.java
+++ src/main/java/com/example/MoneyMind/dtos/UserDTO.java
@@ -1,6 +1,9 @@
 package com.example.MoneyMind.dtos;
 
 
+import com.example.MoneyMind.anotacoes.ContainsCapitalLetter;
+import com.example.MoneyMind.anotacoes.ContainsLowercaseLetter;
+import com.example.MoneyMind.anotacoes.ContainsSpecialCharacter;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -16,11 +19,16 @@ import lombok.Setter;
 public class UserDTO {
     @NotBlank(message = "Please enter a username.")
     private String username;
+
     @Email(message = "Please provide a valid email.")
     @NotBlank(message = "Please provide an email")
     private String email;
+
     @NotBlank(message = "Please enter password")
     @Size(min = 6, max = 20, message = "Password must contain 6 characters")
+    @ContainsCapitalLetter(message = "The password must have at least 1 capital letter")
+    @ContainsLowercaseLetter(message = "Password must have at least 1 lowercase letter")
+    @ContainsSpecialCharacter(message = "Password must have at least 1 special character")
     private String password;
 
     private String cellPhone;
